d534b5abf497d62811fecea8966e16082ba9bdbb
destawardiani <destawardiani@gmail.com>
2020-09-23 21:15:19 +0700

    checkpoint 3

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import firebase from 'firebase';
 
 import Header from './src/components/Header';
+import config from './src/config'
+import LoginForm from './src/components/LoginForm';
 
 class App extends Component {
+  componentDidMount() {
+    if (!firebase.apps.lenght) {
+      console.log('Init Firebase...');
+      firebase.initializeApp(config.firebaseConfig)
+    }
+  }
   render() {
     return (
       <View>
         <Header title="Authentication" />
+
+        <LoginForm />
       </View>
     );
   }
